76cafe021a04d55f6481db19e37f7c389f920fa4
brastlia <120080062+brastli@users.noreply.github.com>
2024-06-06 23:01:28 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,28 +4,81 @@
 // Problem Set: Project 2
 // Main program file for Project 2
 
-#include <iostream>
 #include "Code.h"
 
-using namespace std;
+void findMatches(const dictionary& dict, const Grid& grid) {
+    vector<string> wordsFound;
 
-int main() {
+    int size = grid.getSize();
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            string word;
+            // Horizontal
+            for (int k = j; k < size; ++k) {
+                word += grid.getLetter(i, k);
+                if (dict.wordLookup(word)) {
+                    wordsFound.push_back(word);
+                }
+            }
+            word.clear();
+            // Vertical
+            for (int k = i; k < size; ++k) {
+                word += grid.getLetter(k, j);
+                if (dict.wordLookup(word)) {
+                    wordsFound.push_back(word);
+                }
+            }
+        }
+    }
 
-    // Searches for words in the grid that match those in the dictionary
-    search();
-    Dictionary dict("dictionary.txt"); // Create a Dictionary object with the filename
-    dict.quicksort(0, dict.words.size() - 1); // Sort the words using quicksort
-    // Print the sorted words
-    for (auto word : dict.words) {
+    printWordsFound(wordsFound);
+}
+
+void search() {
+    string dictFile, gridFile;
+    cout << "Enter the dictionary file name: ";
+    cin >> dictFile;
+    cout << "Enter the grid file name: ";
+    cin >> gridFile;
+
+    dictionary dict(dictFile);
+    Grid grid(10);
+    grid.loadFromFile(gridFile);
+
+    findMatches(dict, grid);
+}
+
+void search(int algorithm) {
+    string dictFile, gridFile;
+    cout << "Enter the dictionary file name: ";
+    cin >> dictFile;
+    cout << "Enter the grid file name: ";
+    cin >> gridFile;
+
+    dictionary dict(dictFile);
+    if (algorithm == 1) {
+        dict.quicksort(0, dict.words.size() - 1);
+    } else if (algorithm == 2) {
+        dict.heapsort();
+    }
+
+    Grid grid(10);
+    grid.loadFromFile(gridFile);
+
+    findMatches(dict, grid);
+}
+
+void printWordsFound(vector<string>& wordsFound) {
+    cout << "Words found: " << endl;
+    for (const string& word : wordsFound) {
         cout << word << endl;
     }
-    // Prompt the user to choose the sorting algorithm
-    int algorithm;
-    cout << "Enter 1 for quicksort, 2 for heapsort: ";
-    cin >> algorithm;
+}
 
-    // Perform the search with the chosen algorithm
-    search(algorithm);
+int main() {
+    int choice;
+    cout << "Enter 1 for quicksort, 2 for heapsort, or any other number to skip sorting: ";
+    cin >> choice;
+    search(choice);
     return 0;
-
 }
